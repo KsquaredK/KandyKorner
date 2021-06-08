1d72d09498c9c9420333d020adcfaccd4bc59f4a
KsquaredK <ksquaredkelly@gmail.com>
2021-06-08 14:31:22 -0400

    build out product component

---
+++ src/components/product/Product.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Product.css";
+
+export const Product = () => (
+  <section className="product">
+    <h3 className="product__id">1</h3>
+    <div className="product__name">Rolos</div>
+  </section>
+);
